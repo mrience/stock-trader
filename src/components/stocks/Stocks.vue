685e8ccfaba3dff6cdd0ef8dbc1a489fc7b0cdfb
mateusz <kucka.mateusz14@gmail.com>
2020-03-29 20:33:33 +0200

    add buy button

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -8,20 +8,16 @@
 
 <script>
   import Stock from './Stock'
+
     export default {
-        name: "Stocks",
-        data() {
-          return {
-            stocks: [
-              {id: 1, name:'BMW', price: 110},
-              {id: 2, name:'Google', price: 200},
-              {id: 3, name:'Apple', price: 150},
-              {id: 4, name:'Twitter', price: 80}
-            ]
-          }
-        },
+      name: "Stocks",
       components: {
           appStock: Stock
+      },
+      computed: {
+          stocks() {
+            return this.$store.getters.stocks;
+          }
       }
     }
 </script>
