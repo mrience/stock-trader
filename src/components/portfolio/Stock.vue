1a2c0c1cd720957b4fc50b00c7ab6a9e8ac2f7fc
mateusz <kucka.mateusz14@gmail.com>
2020-04-04 21:53:07 +0200

    fix errors

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -47,16 +47,17 @@
       }
     },
     methods: {
-      ...mapActions([
-        'sellStock'
-      ]),
+      ...mapActions({
+        placeSellOrder: 'sellStock'
+      }),
       sellStock() {
         const order = {
           stockId: this.stock.id,
           stockPrice: this.stock.price,
           quantity: this.quantity
         };
-        this.sellStock();
+        this.placeSellOrder(order);
+        this.quantity = 0;
       }
     }
   }
